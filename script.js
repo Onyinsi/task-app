7090c3fd526032cebcc4b0f4819eca7ebc189e14
OnyinsiSoss <72618136+OnyinsiSoss@users.noreply.github.com>
2022-10-26 09:57:51 +0300

    new updates

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.addEventListener("load", () => {
       
       const task_el = document.createElement("div");
       task_el.classList.add("task");
-      const task_content_el = document.createElement("ol");
+      const task_content_el = document.createElement("li");
       task_content_el.classList.add("content");
   
       task_el.appendChild(task_content_el);
